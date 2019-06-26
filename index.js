0f6d4a663e3f3c8a9266c962b9cb650ef2741950
mkatenolan <mkatenolan@gmail.com>
2019-06-26 12:54:20 +0100

    Add decimal functionality + previousKeyType dataset

--- index.js
+++ index.js
@@ -9,14 +9,20 @@ const key = e.target;
 const action = key.dataset.action;
 const keyType = key.textContent;
 const displayedNum = display.textContent;
+const previousKeyType = calculator.dataset.previousKeyType;
 
 if (!action) {
-  if (displayedNum === "0") {
-    display.textContent = key.textContent;
+  if (displayedNum === '0' || previousKeyType === 'operator') {
+    display.textContent = KeyType;
   } else {
     display.textContent = displayedNum + keyType;
   }
 }
+
+if (action === 'decimal') {
+  display.textContent = displayedNum + '.'
+}
+
 
 
 }
