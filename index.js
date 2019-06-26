9b4f7758bd0fe4fd727f78ad5782d2d126ff4ff3
mkatenolan <mkatenolan@gmail.com>
2019-06-26 17:09:20 +0100

    Add previousKeyType value for buttons

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+//declare main variables
+
 const calculator = document.querySelector ('.calculator');
 const keys = document.querySelector ('.keys');
 const display = document.querySelector ('#display');
 
+//add event listener
+
 keys.addEventListener('click', e=> {
   if (e.target.matches('button')) {
+
+//declare other necessary variables
 
 const key = e.target;
 const action = key.dataset.action;
@@ -11,23 +17,33 @@ const keyType = key.textContent;
 const displayedNum = display.textContent;
 const previousKeyType = calculator.dataset.previousKeyType;
 
+// change display content depending on key type
+
 if (!action) {
   if (displayedNum === '0' || previousKeyType === 'operator') {
     display.textContent = keyType;
   } else {
     display.textContent = displayedNum + keyType;
   }
+  calculator.dataset.previousKeyType = 'number';
 }
 
+// if decimal is pressed
+
 if (action === 'decimal') {
-  display.textContent = displayedNum + '.'
+  display.textContent = displayedNum + '.';
+  calculator.dataset.previousKeyType = 'decimal';
 }
+
+// if an operator is pressed
 
 if (action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
   calculator.dataset.previousKeyType = 'operator';
   calculator.dataset.firstValue = displayedNum;
   calculator.dataset.operator = action;
 }
+
+// basic calculation
 
 const calculate = (n1, operator, n2) => {
   let result = '';
@@ -45,12 +61,21 @@ const calculate = (n1, operator, n2) => {
   return result;
 }
 
+// if equals is pressed
+
 if (action === 'calculate') {
   const firstNum = calculator.dataset.firstValue;
   const secondNum = displayedNum;
   const operator = calculator.dataset.operator;
 
-  display.textContent = calculate (firstNum, operator, secondNum)
+  display.textContent = calculate (firstNum, operator, secondNum);
+  calculator.dataset.previousKeyType = 'calculate';
+}
+
+//if the clear key is pressed 
+
+if (action === 'clear') {
+  calculator.dataset.previousKeyType = 'clear';
 }
 
 }
